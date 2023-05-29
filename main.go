d887a629a9780614c647f653103250f79ad57c55
kijimaD <norimaking777@gmail.com>
2023-05-29 23:21:54 +0900

    feat: use envconfig

--- main.go
+++ main.go
@@ -1,20 +1,36 @@
 package main
 
 import (
+	"fmt"
+	"os"
 	"runtime"
+	"strconv"
 
+	"github.com/kelseyhightower/envconfig"
 	garbanzo "github.com/kijimaD/garbanzo/pkg"
 )
 
+type Env struct {
+	AppPort   uint16 `envconfig:"APP_PORT" default:"8080"`
+	ProxyPort uint16 `envconfig:"PROXY_PORT" default:"8081"` // TODO: 別パッケージに対応してないところがある
+}
+
 func main() {
+	var env Env
+	err := envconfig.Process("", &env)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Can't parse environment variables: %s\n", err.Error())
+		os.Exit(1)
+	}
+
 	go func() {
 		router := garbanzo.NewRouter("pkg/templates/*.html")
-		router.Start(":8080")
+		router.Start(":" + strconv.FormatUint(uint64(env.AppPort), 10))
 	}()
 
 	go func() {
 		proxyrouter := garbanzo.NewProxyRouter()
-		proxyrouter.Start(":8081")
+		proxyrouter.Start(":" + strconv.FormatUint(uint64(env.ProxyPort), 10))
 	}()
 
 	runtime.Goexit()
