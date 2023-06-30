dcedfb83b86f2978b2131d5dd2edc6d632790d71
kijimaD <norimaking777@gmail.com>
2023-07-01 00:20:20 +0900

    refactor: テストをポータブルにする

--- pkg/strutil_test.go
+++ pkg/strutil_test.go
@@ -7,16 +7,25 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestBulidHomeMD(t *testing.T) {
-	homedir, _ := os.UserHomeDir()
-	c := NewConfig(homedir)
-	s, _ := buildTokenStatus(c)
-	assert.Equal(t, true, len(s) > 0)
-}
-
 func TestBulidFeedStatus(t *testing.T) {
 	homedir, _ := os.UserHomeDir()
 	c := NewConfig(homedir)
 	s, _ := buildFeedStatus(c)
 	assert.Equal(t, true, len(s) > 0)
+}
+
+func TestBulidTokenStatus(t *testing.T) {
+	c := NewConfig(".")
+	c.PutConfDir()
+	defer os.RemoveAll(".garbanzo")
+	f, err := os.Create(c.tokenFilePath())
+	defer f.Close()
+	_, err = f.Write([]byte("THIS IS TOKEN"))
+	if err != nil {
+		t.Error(err)
+	}
+
+	s, _ := buildTokenStatus(c)
+	expect := "## GitHub Token\n" + "`~/.garbanzo/token`\n\n" + "🟢 ok"
+	assert.Equal(t, expect, s)
 }
